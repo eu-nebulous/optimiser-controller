aea08deaf2e7e3848c856df399bdd5519e678632
Rudi Schlatte <rudi@ifi.uio.no>
2024-01-15 17:38:16 +0100

    Implement getNodeCandidates
    Change-Id: I907ed7d2bfc23d76624922bc769e159fa2708a48

--- optimiser-controller/src/main/java/eu/nebulouscloud/optimiser/controller/SalConnector.java
+++ optimiser-controller/src/main/java/eu/nebulouscloud/optimiser/controller/SalConnector.java
@@ -2,15 +2,22 @@ package eu.nebulouscloud.optimiser.controller;
 
 import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.annotation.Nulls;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.netty.buffer.ByteBuf;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.logging.LogLevel;
+
+import org.ow2.proactive.sal.model.NodeCandidate;
 import org.ow2.proactive.sal.model.PACloud;
+import org.ow2.proactive.sal.model.Requirement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import reactor.core.publisher.Mono;
+import reactor.netty.ByteBufMono;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
@@ -37,6 +44,7 @@ public class SalConnector {
 
     private static final String connectStr = "sal/pagateway/connect";
     private static final String getAllCloudsStr = "sal/cloud";
+    private static final String findNodeCandidatesStr = "sal/nodecandidates";
 
     private URI sal_uri;
     private final HttpClient httpClient;
@@ -109,6 +117,10 @@ public class SalConnector {
         log.info("Connected to SAL, sessionid {}...", session_id.substring(0, 10));
     }
 
+    /**
+     * Get all cloud providers.  See
+     * https://github.com/ow2-proactive/scheduling-abstraction-layer/blob/master/documentation/2-cloud-endpoints.md#22--getallclouds-endpoint
+     */
     public List<PACloud> getAllClouds() {
         return httpClient.get()
             .uri(sal_uri.resolve(getAllCloudsStr))
@@ -131,4 +143,48 @@ public class SalConnector {
             .map(Arrays::asList)
             .orElseGet(Collections::emptyList);
     }
+
+    /**
+     * Get node candidates.  See
+     * https://github.com/ow2-proactive/scheduling-abstraction-layer/blob/master/documentation/7-node-endpoints.md#71--findnodecandidates-endpoint
+     */
+    public List<NodeCandidate> findNodeCandidates(List<Requirement> requirements) {
+        return httpClient.post()
+            .uri(sal_uri.resolve(findNodeCandidatesStr))
+                .send(bodyMonoPublisher(requirements))
+                .responseSingle((resp, bytes) -> {
+                    if (!resp.status().equals(HttpResponseStatus.OK)) {
+                    return bytes.asString().flatMap(body -> Mono.error(new RuntimeException(body)));
+                    } else {
+                        return bytes.asString().mapNotNull(s -> {
+                            try {
+                                log.info("Received message: {}", s);
+                                return objectMapper.readValue(s, NodeCandidate[].class);
+                            } catch (IOException e) {
+                                log.error(e.getMessage(), e);;
+                                return null;
+                            }
+                        });
+                    }
+                })
+                .doOnError(Throwable::printStackTrace)
+                .blockOptional()
+                .map(Arrays::asList)
+                .orElseGet(Collections::emptyList);
+    }
+
+    private Mono<ByteBuf> bodyMonoPublisher(Object body) {
+        // if ((body instanceof JSONArray) || (body instanceof JSONObject)) {
+        //     return ByteBufMono.fromString(Mono.just(body.toString()));
+        // }
+        String json = null;
+        try {
+            json = objectMapper.writeValueAsString(body);
+        } catch (JsonProcessingException e) {
+            log.error(e.getMessage(), e);;
+        }
+        log.info("Sending body json: {}", json);
+        return ByteBufMono.fromString(Mono.just(json));
+    }
+
 }
