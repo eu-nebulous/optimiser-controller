eu-nebulous/optimiser-controller

commit b0b03aa6d753d774581fcd3510964fc3ab1ec533

Generate (more) AMPL constraints
All entries in `/utilityFunctions` that have `type: constraint` are now used
to generate AMPL constraints.

optimiser-controller/src/main/java/eu/nebulouscloud/optimiser/controller/AMPLGenerator.java
@@ -70,14 +70,23 @@ public class AMPLGenerator {
     }
 
     private static void generateConstraints(NebulousApp app, PrintWriter out) {
-        // We only care about SLOs defined over performance indicators
-        out.println("# Constraints. For constraints we don't have name from GUI, must be created");
+        out.println("# Constraints extracted from `/sloViolations`. For these we don't have name from GUI, must be created");
         int counter = 0;
         for (JsonNode slo : app.getEffectiveConstraints()) {
             out.format("subject to constraint_%d : ", counter);
             emitCondition(out, slo);
             out.println(";");
             counter = counter + 1;
+        }
+        out.println("# Constraints specified with `type: constraint` in `/utilityFunctions`");
+        for (JsonNode c : app.getSpecifiedConstraints()) {
+            String formula = replaceVariables(
+                c.at("/expression/formula").asText(),
+                c.withArray("/expression/variables"));
+            String operator = c.at("/expression/operator").asText();
+            if (operator.equals("==")) operator = "=";
+            out.format("subject to %s :%n	%s %s 0;%n",
+                c.at("/name").asText(), formula, operator);
         }
     }
 
@@ -111,8 +120,6 @@ public class AMPLGenerator {
     private static void generateUtilityFunctions(NebulousApp app, PrintWriter out) {
         out.println("# Utility functions");
         for (JsonNode f : app.getUtilityFunctions().values()) {
-            if (f.get("type").asText().equals("constant"))
-                continue;
             String formula = replaceVariables(
                 f.at("/expression/formula").asText(),
                 f.withArray("/expression/variables"));

optimiser-controller/src/main/java/eu/nebulouscloud/optimiser/controller/NebulousApp.java
@@ -146,6 +146,11 @@ public class NebulousApp {
      * solver, so it should not be included in the AMPL file.
      */
     @Getter private Set<JsonNode> effectiveConstraints = new HashSet<>();
+    /**
+     * The constraints that are specified in the `utilityFunctions` array,
+     * with a `type` of `constraint`.
+     */
+    @Getter private Set<JsonNode> specifiedConstraints = new HashSet<>();
 
     // ----------------------------------------
     // Deployment stuff
@@ -251,7 +256,11 @@ public class NebulousApp {
             log.error("Cannot read parameters from app message, continuing without parameters");
         }
         for (JsonNode f : originalAppMessage.withArray(utility_function_path)) {
-            utilityFunctions.put(f.get("name").asText(), f);
+            if (f.at("/type").asText().equals("constraint")) {
+                specifiedConstraints.add(f);
+            } else {
+                utilityFunctions.put(f.at("/name").asText(), f);
+            }
         }
 
         // We need to know which metrics are raw, composite, and which ones
@@ -295,7 +304,7 @@ public class NebulousApp {
             // What's left is neither a raw nor composite metric.
             utilityFunctions.put(f.get("name").asText(), f);
         }
-        // In the current app message, constraints is not an array.  When this
+        // In the current app message, `constraints` is not an array.  When this
         // changes, wrap this for loop in another loop over the constraints
         // (Constraints are called sloViolations in the app message).
         for (String key : app_message.withObject(constraints_path).findValuesAsText("metricName")) {

optimiser-controller/src/test/java/eu/nebulouscloud/optimiser/controller/NebulousAppTests.java
@@ -48,10 +48,13 @@ public class NebulousAppTests {
     void readValidAppCreationMessage() throws URISyntaxException, IOException {
         NebulousApp app = appFromTestFile("app-creation-message-mercabana.json");
         NebulousApp app2 = appFromTestFile("app-creation-message-complex.json");
+        NebulousApp app3 = appFromTestFile("task-787/app-message-with-var-constraint.json");
         assertNotNull(app);
         assertNotNull(app2);
+        assertNotNull(app3);
         assertTrue(app.validatePaths());
         assertTrue(app2.validatePaths());
+        assertTrue(app3.validatePaths());
     }
 
     @Test

optimiser-controller/src/test/resources/task-787/generic-app-creation-message-with-var-constraints.mod
@@ -1,0 +1,19 @@
+# Variables
+var spec_components_0_traits_0_properties_replicas integer >= 1, <= 5 := 1;
+
+# Metrics.  Note that we only emit metrics that are in use.  Values will be provided by the solver.\n
+## Raw metrics\n
+param AccumulatedSecondsPendingRequests;
+
+## Composite metrics\n\n
+# Constants\n\n
+# Performance indicators = composite metrics that have at least one variable in their formula\n
+# Performance indicator formulas\n\n
+# Cost parameters - for all components, and use of node-candidates tensor\n
+# Utility functions\n
+minimize f :
+AccumulatedSecondsPendingRequests/(spec_components_0_traits_0_properties_replicas*100);
+
+# Default utility function: specified in message to solver\n\n# Constraints. For constraints we don't have name from GUI, must be created\n",
+
+subject to example_constraint: AccumulatedSecondsPendingRequests/(spec_components_0_traits_0_properties_replicas*100) - 100 >= 0;

commit c08ff519b71e2e2531d07e1b46740b60c4f9a27b

Do not emit "constant" entries in AMPL Utility Functions section
Bug introduced in commit b0b03aa6d753d774581fcd3510964fc3ab1ec533

optimiser-controller/src/main/java/eu/nebulouscloud/optimiser/controller/AMPLGenerator.java
@@ -120,6 +120,8 @@ public class AMPLGenerator {
     private static void generateUtilityFunctions(NebulousApp app, PrintWriter out) {
         out.println("# Utility functions");
         for (JsonNode f : app.getUtilityFunctions().values()) {
+            if (f.get("type").asText().equals("constant"))
+                continue;
             String formula = replaceVariables(
                 f.at("/expression/formula").asText(),
                 f.withArray("/expression/variables"));